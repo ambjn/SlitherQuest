8156c96f57d1d6f4a3d74a08406033d556d59f91
Amber Jain <jainanuamber@gmail.com>
2023-05-17 19:42:36 +0530

    updated with moveSnake,gameOver

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,14 +1,15 @@
 import { StyleSheet, SafeAreaView, View } from "react-native";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Colors } from "../styles/colors";
 import { PanGestureHandler } from "react-native-gesture-handler";
 import { Direction, GestureEventType } from "../types/GestureEventType";
 import { Coordinate } from "../types/GestureEventType";
 import Snake from "./Snake";
+import { checkGameOver } from "../utils/checkGameOver";
 
 const SNAKE_INITIAL_POSITION = [{ x: 5, y: 5 }];
 const FOOD_INITIAL_POSITION = { x: 5, y: 20 };
-const GAME_BOUNDS = { xMin: 0, xMax: 35, yMin: 0, yMax: 63 };
+const GAME_BOUNDS = { xMin: 0, xMax: 35, yMin: 0, yMax: 80 };
 const MOVE_INTERVAL = 50;
 const SCORE_INCREMENT = 10;
 
@@ -19,6 +20,48 @@ const Game = (): JSX.Element => {
   const [score, setScore] = useState<number>(0);
   const [isGameOver, setIsGameOver] = useState<boolean>(false);
   const [isPaused, setIsPaused] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (!isGameOver) {
+      const intervalID = setInterval(() => {
+        !isPaused && moveSnake();
+      }, MOVE_INTERVAL);
+      return () => clearInterval(intervalID);
+    }
+  }, [snake, isGameOver, isPaused]);
+
+  const moveSnake = () => {
+    const snakeHead = snake[0];
+    const newHead = { ...snakeHead }; // creating a copy of the snakeHead
+
+    // game-over
+    if(checkGameOver(snakeHead, GAME_BOUNDS)){
+      setIsGameOver((prev) => !prev);
+      return;
+    }
+
+    switch (direction) {
+      case Direction.Up:
+        newHead.y -= 1;
+        break;
+      case Direction.Down:
+        newHead.y += 1;
+        break;
+      case Direction.Left:
+        newHead.x -= 1;
+        break;
+      case Direction.Right:
+        newHead.x += 1;
+        break;
+      default:
+        break;
+    }
+
+    // if it eats food then we want to grow the snake
+
+    // else slice
+    setSnake([newHead, ...snake.slice(0,-1)]);
+  };
 
   const handleGesture = (event: GestureEventType) => {
     const { translationX, translationY } = event.nativeEvent;
@@ -65,7 +108,7 @@ const styles = StyleSheet.create({
     borderColor: Colors.primary,
     borderWidth: 12,
     borderRadius: 30,
-    marginTop:30,
+    marginTop: 30,
     backgroundColor: Colors.background,
   },
 });
