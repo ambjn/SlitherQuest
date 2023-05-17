eac11b59991e1e9a037c7cdc20d63737c92a28ae
Amber Jain <jainanuamber@gmail.com>
2023-05-17 18:59:50 +0530

    updated

--- src/components/Snake.tsx
+++ src/components/Snake.tsx
@@ -1,14 +1,34 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, View } from "react-native";
+import React, { Fragment } from "react";
+import { Coordinate } from "../types/GestureEventType";
+import { Colors } from "../styles/colors";
 
-const Snake = () => {
-  return (
-    <View>
-      <Text>Snake</Text>
-    </View>
-  )
+interface SnakeProps {
+  snake: Coordinate[];
 }
 
-export default Snake
+const Snake = ({ snake }: SnakeProps): JSX.Element => {
+  return (
+    <Fragment>
+      {snake.map((segment: Coordinate, index: number) => {
+        const segmentStyle = {
+          left: segment.x * 10, // +ve & -ve x-axis
+          top: segment.y * 10, // +ve & -ve y-axis
+        };
+        return <View key={index} style={[styles.snake, segmentStyle]}></View>;
+      })}
+    </Fragment>
+  );
+};
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  snake: {
+    width: 15,
+    height: 15,
+    backgroundColor: Colors.primary,
+    borderRadius: 7,
+    position: "absolute",
+  },
+});
+
+export default Snake;
