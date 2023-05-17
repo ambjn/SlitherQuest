0d52c7df69a6b27f3ef81a51ccf7cf1d9c4182a4
Amber Jain <jainanuamber@gmail.com>
2023-05-17 18:32:59 +0530

    added more interfaces & enums

--- src/types/GestureEventType.ts
+++ src/types/GestureEventType.ts
@@ -2,5 +2,14 @@ export interface GestureEventType {
     nativeEvent: {
         translationX: number;
         translationY: number;
-    }
-}
+    };
+};
+
+export interface Coordinate {
+    x: number;
+    y: number;
+};
+
+export enum Direction {
+    Left, Right, Up, Down
+};
