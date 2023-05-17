73abeed8055e1b6c05a36dcecc3ccda4261c150f
Amber Jain <jainanuamber@gmail.com>
2023-05-17 18:21:04 +0530

    worked with co-ordinates tracing, added comments for better understanding

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,14 +1,40 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, SafeAreaView } from "react-native";
+import React from "react";
+import { Colors } from "../styles/colors";
+import { PanGestureHandler } from "react-native-gesture-handler";
 
-const Game = ():JSX.Element => {
+const Game = (): JSX.Element => {
+  const handleGesture = (event: any) => {
+    const { translationX, translationY } = event.nativeEvent;
+    // console.log(translationX, translationY);
+    // use this to understand co-ordinates
+    if (Math.abs(translationX) > Math.abs(translationY)) {
+      // moving on the x-axis
+      if (translationX > 0) {
+        //this means we are moving to RIGHT on the x-axis
+      } else {
+        //this means we are moving to LEFT on the x-axis
+      }
+    } else {
+      // moving on the y-axis
+      if (translationY > 0) {
+        //this means we are moving DOWN on the y-axis
+      } else {
+        //this means we are moving to the UP on the y-axis
+      }
+    }
+  };
   return (
-    <View>
-      <Text>Game</Text>
-    </View>
+    <PanGestureHandler onGestureEvent={handleGesture}>
+      <SafeAreaView style={styles.container}></SafeAreaView>
+    </PanGestureHandler>
   );
-}
+};
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.primary,
+  },
+});
 export default Game;
-
-const styles = StyleSheet.create({})
