8789a7c4d3d2fb79581513d725de1d79254aa95b
Amber Jain <jainanuamber@gmail.com>
2023-05-17 21:47:52 +0530

    random fruits issue fixed

--- src/components/Food.tsx
+++ src/components/Food.tsx
@@ -1,14 +1,18 @@
 import { StyleSheet, Text } from "react-native";
 import { Coordinate } from "../types/GestureEventType";
-
 function getRandomFruitsEmoji() {
   const fruitsEmojis = ["🍎", "🍊", "🍋", "🍇", "🍉", "🍓", "🍑", "🍍"];
   const randomIndex = Math.floor(Math.random() * fruitsEmojis.length);
   return fruitsEmojis[randomIndex];
 }
+let currentFruit = getRandomFruitsEmoji();
 
 export default function Food({ x, y }: Coordinate): JSX.Element {
-  return <Text style={[{ top: y * 10, left: x * 10 }, styles.food]}>🍎</Text>;
+  return (
+    <Text style={[{ top: y * 10, left: x * 10 }, styles.food]}>
+      {currentFruit}
+    </Text>
+  );
 }
 
 const styles = StyleSheet.create({
