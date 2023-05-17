ce3dc01d9a7d2703559eb99749580c22e81e34e5
Amber Jain <jainanuamber@gmail.com>
2023-05-17 18:47:08 +0530

    added Snake component

---
+++ src/components/Snake.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Snake = () => {
+  return (
+    <View>
+      <Text>Snake</Text>
+    </View>
+  )
+}
+
+export default Snake
+
+const styles = StyleSheet.create({})
